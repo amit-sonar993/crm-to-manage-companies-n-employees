fcb140462e5e66a85fec872cad39ad6e0147ff25
Amit Sonar <amit.sonar993@gmail.com>
2023-03-05 11:32:52 +0545

    CompanyController EmployeeController resource controller created Employee and Company model along with migration file created

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -1,18 +1,15 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="{{ route('dashboard') }}" class="brand-link">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+      <span class="brand-text font-weight-light">Dashboard</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
         </div>
@@ -33,7 +30,7 @@
           </li>
           <li class="nav-item">
             <a href="pages/gallery.html" class="nav-link">
-              <i class="nav-icon far fa-image"></i>
+              <i class="fas fa-user bg-info"></i>
               <p>
                 Empoyees
               </p>
