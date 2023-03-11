14724e131390a63a43c34f1eafe249943e05ae86
Amit Sonar <amit.sonar993@gmail.com>
2023-03-11 13:11:10 +0545

    test_new_company_can_be_created_with_valid_data done

--- tests/Feature/CompanyCrudTest.php
+++ tests/Feature/CompanyCrudTest.php
@@ -4,12 +4,16 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Company;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CompanyCrudTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
     /**
      * A basic feature test example.
      */
@@ -21,5 +25,32 @@ class CompanyCrudTest extends TestCase
             ->actingAs($user)->get('/companies/create');
 
         $response->assertStatus(200);
+    }
+
+    public function test_new_company_can_be_created_with_valid_data()
+    {
+        $user = User::factory()->create();
+        $logo = UploadedFile::fake()->image('avatar.jpg', 100, 100);
+
+
+        // creating fake storage
+        Storage::fake('public');
+
+        $response = $this
+            ->actingAs($user)
+            ->from('/companies/create')
+            ->post('/companies', [
+                'name' => 'wrong-password',
+                'email' => 'new-password@gmail.com',
+                'logo' => $logo
+            ]);
+
+        $hashName = $logo->hashName();
+
+        // Assert the file was stored...
+        $this->assertTrue(Storage::disk('public')->exists($hashName));
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/companies');
     }
 }
